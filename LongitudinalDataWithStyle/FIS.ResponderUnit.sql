28ad2656ebe2ed282f6398a1d8e508de682e5a18
EMS OFFICE <micah.fullerton@portlandoregon.gov>
2025-02-28 12:08:30 -0800

    updated

---
+++ LongitudinalDataWithStyle/FIS.ResponderUnit.sql
@@ -0,0 +1,12 @@
+SELECT TOP (1000)
+
+    [ResponderUnit_ID]
+    ,[Description]
+    ,[ID_911]
+    ,[Station_ID]
+    ,[TranslateTo]
+    ,[Agency_ID]
+    ,[Process]
+    ,[VersaTerm]
+    
+  FROM [Incident].[dbo].[ResponderUnit]
